936958a864ba1c1f869ce8db3aa4da8a1b137b50
Nick Polishchuk <101862547+plsmrpostman@users.noreply.github.com>
2022-05-25 17:19:40 +0300

    init

---
+++ precache-manifest.574d5aac7a4cea76fe398d9157df2228.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df83461209c5308def07",
+    "url": "/centum-d-preview/css/app.6d4a7b3d.css"
+  },
+  {
+    "revision": "c29dfd1a92849df3ee85",
+    "url": "/centum-d-preview/css/chunk-d694ed98.81269e80.css"
+  },
+  {
+    "revision": "7700e65a98fe1a26d01c",
+    "url": "/centum-d-preview/css/chunk-vendors.9d780509.css"
+  },
+  {
+    "revision": "086d23b119506bddf90dc47632dafb44",
+    "url": "/centum-d-preview/index.html"
+  },
+  {
+    "revision": "df83461209c5308def07",
+    "url": "/centum-d-preview/js/app.e4410f3f.js"
+  },
+  {
+    "revision": "c29dfd1a92849df3ee85",
+    "url": "/centum-d-preview/js/chunk-d694ed98.3e553f3c.js"
+  },
+  {
+    "revision": "7700e65a98fe1a26d01c",
+    "url": "/centum-d-preview/js/chunk-vendors.8cdf9d09.js"
+  },
+  {
+    "revision": "24a7fb8e9179fc2437c693a60302cc4a",
+    "url": "/centum-d-preview/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/centum-d-preview/robots.txt"
+  }
+]);
